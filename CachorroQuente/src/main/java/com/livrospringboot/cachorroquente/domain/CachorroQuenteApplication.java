287a36e40f0e59538fb21fb5ec72078ee769f731
FerrazVinicius96 <vesf.96@gmail.com>
2026-03-05 19:04:49 -0300

    Project running

--- CachorroQuente/src/main/java/com/livrospringboot/cachorroquente/domain/CachorroQuenteApplication.java
+++ CachorroQuente/src/main/java/com/livrospringboot/cachorroquente/domain/CachorroQuenteApplication.java
@@ -1,4 +1,4 @@
-package com.livrospringboot.cachorroquente;
+package com.livrospringboot.cachorroquente.domain;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- CachorroQuente/src/main/java/com/livrospringboot/cachorroquente/domain/Propriedade.java
+++ CachorroQuente/src/main/java/com/livrospringboot/cachorroquente/domain/Propriedade.java
@@ -1,17 +1,22 @@
-package com.livrospringboot.cachorroquente.domain.entities;
+package com.livrospringboot.cachorroquente.domain;
 
 import jakarta.persistence.Entity;
+
 import jakarta.persistence.Id;
+
+import java.util.Objects;
 
 @Entity
 public class Propriedade {
 
     @Id
     private String nome;
+
     private String valor;
+
     private String descricao;
+
     private String categoria;
-    private String subcategoria;
 
     public String getNome() {
         return nome;
@@ -45,11 +50,27 @@ public class Propriedade {
         this.categoria = categoria;
     }
 
-    public String getSubcategoria() {
-        return subcategoria;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Propriedade that = (Propriedade) o;
+        return Objects.equals(nome, that.nome);
     }
 
-    public void setSubcategoria(String subcategoria) {
-        this.subcategoria = subcategoria;
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome);
     }
+
+    @Override
+    public String toString() {
+        return "Propriedade{" +
+                "nome='" + nome + '\'' +
+                ", valor='" + valor + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", categoria='" + categoria + '\'' +
+                '}';
+    }
+
 }
